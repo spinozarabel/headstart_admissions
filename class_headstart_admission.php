5b45a605da2896a30b00e468a0a4419af1d57ba8
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-07-18 08:00:56 +0530

    dev

--- class_headstart_admission.php
+++ class_headstart_admission.php
@@ -118,11 +118,11 @@ class class_headstart_admission
 
         $admin_label_array =array_column(array_column($fields_ninjaforms, 'settings'), 'admin_label');
 
-        // error_log("admin label array: " . print_r($admin_label_array, true));
+        error_log("admin label array: " . print_r($admin_label_array, true));
 
         $keymap = array_keys($fields_ninjaforms);
 
-        // error_log("admin label keymap: " . print_r($keymap, true));
+        error_log("admin label keymap: " . print_r($keymap, true));
 
         $ticket_fields = get_terms([
             'taxonomy'   => 'wpsc_ticket_custom_fields',
