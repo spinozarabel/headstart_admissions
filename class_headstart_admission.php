cf58e044b5c17f52a5cc4b2f3253b43e93f79bae
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-07-18 07:11:33 +0530

    dev

--- class_headstart_admission.php
+++ class_headstart_admission.php
@@ -118,7 +118,11 @@ class class_headstart_admission
 
         $admin_label_array =array_column(array_column($fields_ninjaforms, 'settings'), 'admin_label');
 
+        error_log("admin label array: " . print_r($admin_label_array, true));
+
         $keymap = array_keys($fields_ninjaforms);
+
+        error_log("admin label keymap: " . print_r($keymap, true));
 
         $ticket_fields = get_terms([
             'taxonomy'   => 'wpsc_ticket_custom_fields',
@@ -270,7 +274,7 @@ class class_headstart_admission
         endforeach;
 
         // we have all the necessary ticket fields filled from the Ninja forms, now we can create a new ticket
-        $ticket_id = $wpscfunction->create_ticket($ticket_args);
+        //$ticket_id = $wpscfunction->create_ticket($ticket_args);
     }
 
 
