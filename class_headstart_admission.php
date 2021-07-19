4cb770901d4c64b1cea4bf9b41fbbad84e78228a
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-07-20 03:09:00 +0530

    dev

--- class_headstart_admission.php
+++ class_headstart_admission.php
@@ -524,7 +524,7 @@ class class_headstart_admission
     {
         // tthe following piece of code gets executed and results displayed in the  SriToni tools page when button is pressed
         global $wpscfunction;
-        $ticket_id = 4;
+        $ticket_id = 8;
         $ticket_data = $wpscfunction->get_ticket($ticket_id);
         echo "<pre>" . print_r($ticket_data, true) ."</pre>";
     }
@@ -533,7 +533,7 @@ class class_headstart_admission
     {
         // the folowing piece of code dumps details of ticket id =1;
         global $wpscfunction;
-        $ticket_id = 4;
+        $ticket_id = 8;
 
         $fields = get_terms([
             'taxonomy'   => 'wpsc_ticket_custom_fields',
