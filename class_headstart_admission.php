c3163f7efe7b694e5b1201229f9f0ceadddda934
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-07-20 16:37:06 +0530

    dev

--- class_headstart_admission.php
+++ class_headstart_admission.php
@@ -524,7 +524,7 @@ class class_headstart_admission
 
             $params = array(
                                 "role"  => "subscriber",
-                                "email" => "sritoni1@headstart.edu.in",
+                                "email" => "sriton21@headstart.edu.in",
                             );
 
             $customers = $woocommerce->get($endpoint, $params);
