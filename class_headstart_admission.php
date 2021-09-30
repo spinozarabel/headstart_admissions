479731399218bda8ee34de0cc9abec622cfbbdc3
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-09-30 20:58:21 +0530

    criteria correction

--- class_headstart_admission.php
+++ class_headstart_admission.php
@@ -1546,7 +1546,11 @@ class class_headstart_admission
         $MoodleRest->setReturnFormat(MoodleRest::RETURN_ARRAY); // Array is default. You can use RETURN_JSON or RETURN_XML too.
 
         // get moodle user details associated with this username, we need the id to updatethe user's data
-        $parameters   = array("criteria" => array(array("key" => "email", "value" => $moodle_email)));
+        $criteria   = array(array(
+                                    "key"   => "username", 
+                                    "value" => $moodle_username
+                                    )
+                            );
 
         // get moodle user satisfying above criteria if any
         $moodle_users = $MoodleRest->request('core_user_get_users', $parameters, MoodleRest::METHOD_GET);
