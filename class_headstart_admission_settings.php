272559e2aa328f828649c628fc696300440bc7b3
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-11-04 16:24:22 +0530

    added comments for cohort id in settings

--- class_headstart_admission_settings.php
+++ class_headstart_admission_settings.php
@@ -195,7 +195,7 @@ class class_headstart_admission_settings {
     $value = esc_attr( $settings[$field] );
 
     echo "<textarea name='headstart_admission_settings[$field]' id='headstart_admission_settings[$field]'
-            rows='15' cols='125'>" . $value . "</textarea>Do not enter extra spaces, new lines";
+            rows='15' cols='125'>" . $value . "</textarea>Do not enter extra spaces or new lines. The Cohort ID an integer as seen in the URL when editing the cohort";
 
     }
 
